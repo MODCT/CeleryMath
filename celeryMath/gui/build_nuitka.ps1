a2414be9c8ec1e79e67eb5225ff910ddebf7cfe6
rainyl <rainyliusy3@gmail.com>
2022-06-18 20:30:50 +0800

    :axe: fix screenshot on multi-monitor

--- celeryMath/gui/build_nuitka.ps1
+++ celeryMath/gui/build_nuitka.ps1
@@ -3,14 +3,15 @@ nuitka --mingw64 --standalone --output-dir=build `
 --plugin-enable=pyside6 `
 --plugin-enable=numpy `
 --nofollow-import-to=matplotlib `
+--nofollow-import-to=PySide6.QtQml,PySide6.QtQuick `
 --windows-icon-from-ico="resources/icons/logo.ico" `
 --windows-company-name="rainyl.com" `
 --windows-product-name="CeleryMath" `
---windows-file-version="0.1.0.0" `
---windows-product-version="0.1.0.0" `
+--windows-file-version="0.1.1.0" `
+--windows-product-version="0.1.1.0" `
 --windows-file-description="CeleryMath" `
+--windows-disable-console `
 ./celeryMath.py
-# --windows-disable-console `
 
 New-Item build/celeryMath.dist/conf -ItemType Directory -ea 0
 New-Item build/celeryMath.dist/log -ItemType Directory -ea 0
--- celeryMath/gui/src/celeryScreenShotWidget.py
+++ celeryMath/gui/src/celeryScreenShotWidget.py
@@ -8,13 +8,27 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QPoint, QRect, Qt)
-from PySide6.QtGui import (QBrush, QColor, QCursor, QKeyEvent,
-    QMouseEvent, QPaintEvent, QPen, QPainter, QPixmap)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QSizePolicy,
-    QWidget, QMainWindow, QVBoxLayout, QGraphicsOpacityEffect)
+import PySide6
+from PySide6.QtCore import QPoint, QRect, Qt
+from PySide6.QtGui import (
+    QColor,
+    QCursor,
+    QKeyEvent,
+    QMouseEvent,
+    QPaintEvent,
+    QPen,
+    QPainter,
+)
+from PySide6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLabel,
+    QMainWindow,
+)
 
 from PIL import ImageGrab
+
+from pynput.mouse import Controller as MouseController
 
 from .logger import get_logger
 
@@ -24,64 +38,55 @@ QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 class CeleryScreenShotWidget(QMainWindow):
     logger = get_logger("screen_image")
-    img_ltop = QPoint()
-    img_rbot = QPoint()
+    draw_ltop = QPoint()
+    draw_rbot = QPoint()
+    img_ltop = (0, 0)
+    img_rbot = (0, 0)
     is_snipping = False
+
     def __init__(self, parent=None) -> None:
         super(CeleryScreenShotWidget, self).__init__()
         self.parent = parent
-        self.setupUi()
 
         self.setWindowFlags(Qt.WindowStaysOnTopHint)
         self.setWindowFlags(Qt.FramelessWindowHint)
-        QApplication.setOverrideCursor(QCursor(Qt.CrossCursor))
-
-        self.opacity_effect = QGraphicsOpacityEffect()
-
-        # setting opacity level
-        self.opacity_effect.setOpacity(0.3)
-
-    def setupUi(self):
-        if not self.objectName():
-            self.setObjectName(u"screenShotWidget")
-        self.resize(400, 300)
-        self.gridLayout = QGridLayout(self)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.label_image = QLabel(self)
-        self.label_image.setObjectName(u"label_image")
-        self.label_image.setCursor(QCursor(Qt.CrossCursor))
-        self.label_image.setText(u"")
-        self.label_image.setGeometry(0, 0, 400, 300)
-        self.gridLayout.addWidget(self.label_image, 0, 0, 1, 1)
-    # setupUi
+        self.setCursor(QCursor(Qt.CrossCursor))
+        self.mouse = MouseController()
 
     def set_image(self):
         screens = QApplication.screens()
         self.logger.debug(screens)
+        xs, ys = [], []
         for screen in screens:
             rect = screen.geometry()
             scale = screen.devicePixelRatio()
             top_left, bot_right = rect.topLeft(), rect.bottomRight()
             x0, y0 = top_left.x(), top_left.y()
             x1, y1 = bot_right.x(), bot_right.y()
-            self.move(x0, y0)
-            self.resize(x1-x0, y1-y0)
-            self.logger.debug(f"screen: [{screen}], geo: [{x0}, {y0}, {x1}, {y1}]")
-            bbox = (int(x0*scale), int(y0*scale), int(x1*scale), int(y1*scale))
-            img = ImageGrab.grab(bbox=bbox, all_screens=True)
-            im = QPixmap(img.toqimage())
-            self.label_image.setGeometry(x0, y0, x1-x0, y1-y0)
-            self.label_image.setScaledContents(True)
-            self.label_image.setPixmap(im)
+            xs.extend([x0, x1])
+            ys.extend([y0, y1])
+            self.logger.debug(
+                f"screen: [{screen}], scale: [{scale}] geo: [{x0}, {y0}, {x1}, {y1}]"
+            )
+        tl_x, tl_y = min(xs), min(ys)
+        rb_x, rb_y = max(xs), max(ys)
+        new_size = (rb_x - tl_x, rb_y - tl_y)
+        self.move(tl_x, tl_y)
+        self.logger.debug(f"moved to {tl_x}, {tl_y}")
+        self.resize(new_size[0], new_size[1])
+        self.logger.debug(
+            f"resized to {new_size}",
+        )
 
     def reset_rect(self):
-        self.img_ltop.setX(0)
-        self.img_ltop.setY(0)
-        self.img_rbot.setX(0)
-        self.img_rbot.setY(0)
+        self.draw_ltop = QPoint()
+        self.draw_rbot = QPoint()
+        self.img_ltop = (0, 0)
+        self.img_rbot = (0, 0)
 
     def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key_Escape:
+            self.is_snipping = False
             QApplication.restoreOverrideCursor()
             self.reset_rect()
             self.close()
@@ -98,37 +103,31 @@ class CeleryScreenShotWidget(QMainWindow):
         self.raise_()
 
     def mousePressEvent(self, event: QMouseEvent):
-        self.img_ltop = event.pos()
-        self.img_rbot = event.pos()
+        self.draw_ltop = event.pos()
+        self.draw_rbot = event.pos()
+        self.img_ltop = self.mouse.position
+        self.img_rbot = self.img_ltop
         # self.logger.debug(f"press: {self.img_ltop}")
         self.update()
 
     def mouseMoveEvent(self, event: QMouseEvent):
-        self.img_rbot = event.pos()
+        self.draw_rbot = event.pos()
+        self.img_rbot = self.mouse.position
         # self.logger.debug(f"move, {self.img_rbot}")
         self.update()
 
     def mouseReleaseEvent(self, event: QMouseEvent):
         # self.logger.debug(f"button: {event.button()}")
+        self.is_snipping = False
         if event.button() != Qt.MouseButton.LeftButton:
             self.reset_rect()
             self.close()
             self.parent.on_sc_returned()
             return
         self.logger.debug(f"release, {self.img_ltop}, {self.img_rbot}")
-        self.is_snipping = False
-        QApplication.restoreOverrideCursor()
 
-        start = self.img_ltop
-        end = self.img_rbot
-
-        x0 = start.x()
-        y0 = start.y()
-        x1 = end.x()
-        y1 = end.y()
-        screen = QApplication.screenAt(end)
-        scale = screen.devicePixelRatio()
-        bbox = (int(x0*scale), int(y0*scale), int(x1*scale), int(y1*scale))
+        bbox = (*self.img_ltop, *self.img_rbot)
+        # self.logger.debug(f"bbox: {bbox}")
 
         self.repaint()
         QApplication.processEvents()
@@ -140,18 +139,16 @@ class CeleryScreenShotWidget(QMainWindow):
         self.parent.on_sc_returned(img)
 
     def paintEvent(self, event: QPaintEvent):
-        # self.logger.debug(f"paint, {self.is_snipping}",)
         if self.is_snipping:
             brushColor = "#b1d5c8"
             lw = 1
-            opacity = 0.8
+            opacity = 0.2
         else:
             brushColor = "#FFFFFF"
             lw = 1
-            opacity = 1
-        self.opacity_effect.setOpacity(opacity)
-        self.label_image.setGraphicsEffect(self.opacity_effect)
+            opacity = 0.0
+        self.setWindowOpacity(opacity)
         qp = QPainter(self)
         qp.setPen(QPen(QColor("#000000"), lw))
         qp.setBrush(QColor(brushColor))
-        qp.drawRect(QRect(self.img_ltop, self.img_rbot))
+        qp.drawRect(QRect(self.draw_ltop, self.draw_rbot))
