9368cc8b163de0c10469084f0d8773bfea5c5d5a
rainyl <rainyliusy3@gmail.com>
2022-06-19 15:19:43 +0800

    :pencil: add single instance

--- celeryMath/gui/build_nuitka.ps1
+++ celeryMath/gui/build_nuitka.ps1
@@ -4,11 +4,12 @@ nuitka --mingw64 --standalone --output-dir=build `
 --plugin-enable=numpy `
 --nofollow-import-to=matplotlib `
 --nofollow-import-to=PySide6.QtQml,PySide6.QtQuick `
+--nofollow-import-to=black `
 --windows-icon-from-ico="resources/icons/logo.ico" `
---windows-company-name="rainyl.com" `
+--windows-company-name="rainyl@MODCT.org" `
 --windows-product-name="CeleryMath" `
---windows-file-version="0.1.1.0" `
---windows-product-version="0.1.1.0" `
+--windows-file-version="0.1.2.0" `
+--windows-product-version="0.1.2.0" `
 --windows-file-description="CeleryMath" `
 --windows-disable-console `
 ./celeryMath.py
--- celeryMath/gui/celeryMath.py
+++ celeryMath/gui/celeryMath.py
@@ -1,13 +1,39 @@
 import sys
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtCore import QSystemSemaphore, QSharedMemory
 
 from src.application import CeleryMath
 from src.lib.utils.utils import seed_everything
 
+WIN_ID = "celeryMath"
+SRE_MEM_ID = "celeryMathMem"
+
 
 def main():
     app = QApplication(sys.argv)
+
+    semaphore = QSystemSemaphore(WIN_ID, 1)
+    semaphore.acquire()
+    if sys.platform != "win32":
+        nix_fix_shared_mem = QSharedMemory(SRE_MEM_ID)
+        if nix_fix_shared_mem.attach():
+            nix_fix_shared_mem.detach()
+    shared_memory = QSharedMemory(SRE_MEM_ID)
+    if shared_memory.attach():
+        is_running = True
+    else:
+        shared_memory.create(1)
+        is_running = False
+    semaphore.release()
+    if is_running:
+        QMessageBox.warning(
+            None,
+            "Application already running",
+            "One instance of the application is already running.",
+            QMessageBox.Ok,
+        )
+        return
     mainWindow = CeleryMath()
     # mainWindow.flashSplash()
     mainWindow.show()
